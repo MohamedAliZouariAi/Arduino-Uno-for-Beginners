86d324ee260a50976c737604a2458a74470cec08
Mohamed Ali Zouari <mohamedalizouari.ai@gmail.com>
2024-12-04 22:54:00 +0100

    Add files via upload

---
+++ Project 3/Red_yellow_green_blue.ino
@@ -0,0 +1,28 @@
+
+int Blue = 4;
+int Green = 5;
+int Yellow = 6;
+int Red = 7;
+
+void setup() {
+  // initialize digital pins 4, 5, 6, 7 as  outputs.
+  pinMode(Blue, OUTPUT);
+  pinMode(Green, OUTPUT);
+  pinMode(Yellow, OUTPUT);
+  pinMode(Red, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() 
+{
+  digitalWrite(Blue, HIGH);    // turn the LEDs on (HIGH is the voltage level)
+  digitalWrite(Green, HIGH);
+  digitalWrite(Yellow, HIGH);
+  digitalWrite(Red, HIGH);
+  delay(500);                 // wait for a 0.5 s
+  digitalWrite(Blue, LOW);    // turn the LEDs on (HIGH is the voltage level)
+  digitalWrite(Green, LOW);
+  digitalWrite(Yellow, LOW);
+  digitalWrite(Red, LOW);     // turn the LEDs off by making the voltage LOW
+  delay(500);                 // wait for a 0.1 s
+}
